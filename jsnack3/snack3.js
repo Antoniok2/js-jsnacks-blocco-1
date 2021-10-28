2d586c767c514188fdca617605c272a9184892b2
Antoniok2 <antoniopiazza18@gmail.com>
2021-10-28 15:19:08 +0200

    esercizio 3 risolto

---
+++ jsnack3/snack3.js
@@ -0,0 +1,17 @@
+// 3 - Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+let num1 = parseInt(prompt("Inserisci qui un numero"));
+let num2 = parseInt(prompt("Inserisci qui un numero"));
+let num3 = parseInt(prompt("Inserisci qui un numero"));
+let num4 = parseInt(prompt("Inserisci qui un numero"));
+let num5 = parseInt(prompt("Inserisci qui un numero"));
+let num6 = parseInt(prompt("Inserisci qui un numero"));
+let num7 = parseInt(prompt("Inserisci qui un numero"));
+let num8 = parseInt(prompt("Inserisci qui un numero"));
+let num9 = parseInt(prompt("Inserisci qui un numero"));
+let num10 = parseInt(prompt("Inserisci qui un numero"));
+
+let sommaTot = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+
+alert("La somma totale dei numeri che hai scritto è: " + sommaTot);
